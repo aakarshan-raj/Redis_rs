2449eaa7c4312b67a40cfa8b62f43418ad4e42d4
aakarshan-raj <aakarshan7raj@gmail.com>
2023-10-02 15:54:29 +0530

    listening to connections

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod store;
 
+use std::{net::TcpListener, sync::Arc};
+
 use store::db::Cache;
 
 fn main() {
 
-    let x = Cache::new();
-    println!("{:?}",x);
+    let connections = TcpListener::bind("127.0.0.1:8907").unwrap();
+
+    let cache = Arc::new(Cache::new());
+
+    for connection in connections.incoming(){
+        println!("{:?}",connection);
+    }
 
 }
--- src/store/db.rs
+++ src/store/db.rs
@@ -1,22 +1,50 @@
-use std::{sync::{Arc, RwLock}, collections::HashMap, time::Instant};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+    thread,
+    time::{Duration, Instant},
+};
 
-
-#[derive(Debug,Clone)]
-pub struct Cache{
-   data:Arc<RwLock<HashMap<String,String>>>,
-   expiration:Arc<RwLock<HashMap<String,Instant>>>
-
+#[derive(Debug, Clone)]
+pub struct Cache {
+    data: Arc<RwLock<HashMap<String, String>>>,
+    expiration: Arc<RwLock<HashMap<String, Instant>>>,
 }
 
+impl Cache {
+    pub fn new() -> Self {
+        let data = Arc::new(RwLock::new(HashMap::new()));
+        let expiration = Arc::new(RwLock::new(HashMap::new()));
+        let cache = Cache { data, expiration };
 
-impl Cache{
+        let data_clone = cache.data.clone();
+        let expiration_clone = cache.expiration.clone();
 
-   pub fn new()->Self{
+        thread::spawn(move || loop {
+            let now = Instant::now();
+            let expire: Vec<String> = {
+                let ex = expiration_clone.read().unwrap();
+                ex.iter()
+                    .filter_map(|(key, &time)| {
+                        if time <= now {
+                            return Some(key.clone());
+                        } else {
+                            return None;
+                        }
+                    })
+                    .collect()
+            };
 
-      let data = Arc::new(RwLock::new(HashMap::new()));
-      let expiration = Arc::new(RwLock::new(HashMap::new()));
-      Cache { data , expiration }
+            if !expire.is_empty() {
+                let mut data = data_clone.write().unwrap();
 
-   }
+                for x in expire {
+                    data.remove(&x);
+                }
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
 
+        cache
+    }
 }
